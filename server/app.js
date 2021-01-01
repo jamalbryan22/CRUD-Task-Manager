c3102bebc930d86c695d9120a28ee92ba4bd9059
jamalbryan22 <jamal_bryan@knights.ucf.edu>
2021-01-01 10:43:49 -0500

    created project build

--- server/app.js
+++ server/app.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors")
 require("./db/mongoose");
 const userRouter = require("./router/user");
 const taskRouter = require("./router/task");
-const cors = require("cors")
 
 // Create server and port
 const app = express();
 
 // Run Middleware
+const buildPath = path.join(__dirname, "..", "client", "build");
+console.log(`The public path is ${buildPath}`);
+const publicPath = path.join(__dirname,"..", "client", "public");
+// console.log(`The public path is ${publicPath}`);
+app.use(express.static(buildPath));
+// app.use(express.static(publicPath));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use(cors());
+
+// app.get("/", (req, res) => {
+//          res.sendFile(path.join(publicPath, "index.html"));
+//        });
+
 
 module.exports = app;
 
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 // Connect to DB on localhost
-mongoose.connect(process.env.MONGODB_URL, {
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+// });
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useUnifiedTopology: true 
 });
 
 
