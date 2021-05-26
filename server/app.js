25ad0d1b40517b6f1995272c24f9517f7e8addee
jamalbryan22 <jamal_bryan@knights.ucf.edu>
2021-05-25 21:28:19 -0400

    enabling cors

--- server/app.js
+++ server/app.js
@@ -4,12 +4,19 @@ const cors = require("cors")
 require("./db/mongoose");
 const userRouter = require("./router/user");
 const taskRouter = require("./router/task");
+const corsOptions = {
+  origin: 
+    "https://bryan-task-manager.herokuapp.com"
+  ,
+  credentials: true,
+  exposedHeaders: ["set-cookie"]
+};
 
 // Create server and port
 const app = express();
 
 // Run Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 const buildPath = path.join(__dirname, "..", "client", "build");
 app.use(express.static(buildPath));
 app.use(express.json());
