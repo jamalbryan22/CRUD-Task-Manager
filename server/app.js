037ae1aa93140b8cdee835c5281e889cee1d3e3d
jamalbryan22 <jamal_bryan@knights.ucf.edu>
2021-06-09 03:04:30 -0400

    db  fix

--- server/app.js
+++ server/app.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors")
+const cookieParser = require('cookie-parser')
 require("./db/mongoose");
 const userRouter = require("./router/user");
 const taskRouter = require("./router/task");
-
+const corsOptions = {
+  origin: [
+    "https://bryan-task-manager.herokuapp.com"
+  ],
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+};
 
 // Create server and port
 const app = express();
 
 //Configure CORS use
-app.use(cors())
+app.use(cors(corsOptions))
 
 // Run Middleware
 const buildPath = path.join(__dirname, "..", "client", "build");
 app.use(express.static(buildPath));
 app.use(express.json());
+app.use(cookieParser());
 app.use(userRouter);
 app.use(taskRouter);
 
